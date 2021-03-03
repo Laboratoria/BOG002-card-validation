1d77b2ea17807c733a771231cfc3a5fefce5be69
unknown <lorena0717@hotmail.com>
2021-03-03 11:47:40 -0500

    union codigo

--- src/index.js
+++ src/index.js
@@ -27,37 +27,3 @@ function darClick(){
 
 //console.log(validator)//
 
-<<<<<<< HEAD
-=======
-console.log(validator);
-
-
-    
-let creditCardNumber = 4356789024517953;
-
-
-
-
-let reverseCardNumber = validator.luhnAlgorithm(creditCardNumber);
-
-console.log ('resultado -->' , reverseCardNumber);
-
-let botonValidar = document.getElementById("validate");
-botonValidar.onclick = darClick;
-
-
-
- function darClick(){
-    let IngresarNumero= document.getElementById("numeroDeTarjeta").value;
-    console.log(nombre);
-
-  }
-
-
-    
-
-
-//let mensaje = "esto, es, un, mensaje Hola!"
-//let splitMensaje = mensaje.split(',')
-//console.log(splitMensaje)
->>>>>>> 5ced9fec0e4ec16c8198fe06cab31b4805c283ef
--- src/validator.js
+++ src/validator.js
@@ -1,7 +1,6 @@
 
 const validator = {
   luhnAlgorithm,
-<<<<<<< HEAD
 };
 
 function invertirNumero (numero) {
@@ -9,25 +8,10 @@ function invertirNumero (numero) {
   let arrayNumber = stringNumber.split('');//array es un conjunto de datos en formato numero/split divide la cadena de caracteres
   let reverseArrayNumber=arrayNumber.reverse();//aplicamos la funcion reverse a nuestro array
  return reverseArray;
-=======
-
-};
-
-
-function invertirNumero(numero){
-  let stringNumber = numero.toString();
-  
-  let arrayNumber = stringNumber.split('');
-
-  let reverseArray = arrayNumber.reverse();
-
-  return reverseArray;
->>>>>>> 5ced9fec0e4ec16c8198fe06cab31b4805c283ef
 }
 
 
 function luhnAlgorithm(numero){
-<<<<<<< HEAD
   let reverseArrayNumber = invertirNumero(numero);
   console.log("Reverse Array -->", reverseArrayNumber);
 
@@ -82,74 +66,6 @@ function luhnAlgorithm(numero){
   
   console.log(resultadoSuma)    
   return''
-=======
-
-  let reverseArrayNumber = invertirNumero(numero);
-
-  console.log("Reverse Array --> ", reverseArrayNumber);
-
-  //let reverseNumber = reverseArrayNumber.join('');
-  //console.log("joinReverseNumber -->" , reverseNumber)
-
-  for (let i = 0; i <= reverseArrayNumber.length -1 ; i++) {
-      console.log("posicion --->", i , ' val ->' , reverseArrayNumber[i])
-      let validarnumeros = i%2; //residuo de la posicion dividido en 0 (0 par 1 impar)
-     
-      if(validarnumeros === 0 ){
-         
-         // console.log('la posicion : ', i , 'es PAR');
-         
-          let valor = reverseArrayNumber[i];
-
-          let valorx2 = valor*2;
-              
-          if(valorx2 >= 10){
-              // se debe desglosar el numero y sumar
-              // console.log(valorx2)
-              let num1 = Number(valorx2.toString().split('')[0]);
-              let num2 = Number(valorx2.toString().split('')[1]);
-
-              console.log('num1 -> ', num1);
-              console.log('num2 -> ', num2);
-              
-              let sumaNume= num1 + num2;
-
-              reverseArrayNumber[i] = sumaNume;
-          
-          }else {
-              reverseArrayNumber[i] = valorx2;
-          }
-         
-      }
-      console.log("Posicion "+ i +" valor --> "+ reverseArrayNumber[i]);
-
-  }
-  console.log("Array con validacion de pares --> ", reverseArrayNumber);
-  //console.log("Array pos 2 -->" , arrayNumber[2] )
-
-  let resultadoSuma = 0;
-
-  for (let i = 0; i < reverseArrayNumber.length; i++){
-      resultadoSuma = resultadoSuma + Number(reverseArrayNumber[i]); // 6,5,6
-      
-   
-  }  
-  let resultadoFinal
-  resultadoFinal =resultadoSuma%10;
-
-  console.log('resultado suma -> ', resultadoSuma);
-  console.log('resultado Final -> ', resultadoFinal);
-
-
-
-  //let result = Number(reverseNumber) 
-
-  return resultadoSuma 
-  
-  
-}
-  
->>>>>>> 5ced9fec0e4ec16c8198fe06cab31b4805c283ef
 
 
 export default validator;
