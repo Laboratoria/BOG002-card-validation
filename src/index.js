13b1eb575d9239503e6463db20fb47576ba1f09a
Evelynbotia <evelynbotia07@gmail.com>
2021-03-03 13:19:41 -0500

    cambiosEnFucionClick

--- src/index.js
+++ src/index.js
@@ -18,24 +18,10 @@ function darClick(){
 
 
  
- 
  console.log (valor);
  console.log(invertirNumero(valor));
 
  
-//import validator from './validator.js'//
+//import validator from ' ./validator.js//
 
-
-function darClick(){
-    let IngresarNumeroTar= document.getElementById("numeroDeTarjeta").value;
-    console.log(IngresarNumeroTar);
-
-}
-
-
-    
-
-
-//let mensaje = "esto, es, un, mensaje Hola!"
-//let splitMensaje = mensaje.split(',')
-//console.log(splitMensaje)
+//console.log(validator)//
