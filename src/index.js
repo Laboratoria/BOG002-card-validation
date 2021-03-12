57f18e0a5df57ec5cbd27af6f57e52758ee4b947
unknown <lorena0717@hotmail.com>
2021-03-12 16:39:24 -0500

    cambios maskify y tets aprobados

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 
 import validator from "./validator.js";
-//import * as validator from "./validator.js";
-
-//esta funcion contiene las variables para revesar nuestro valor
-//alert('prueba call')
-
-let numeroDeTarjeta =document.getElementById("numeroDeTarjeta");// creamos una variable para llamar el valor del id de numeroDeTarjeta.
-numeroDeTarjeta.addEventListener("keyup", restringirTarjeta)// traemos el valor llamado para que cumpla la funcion restringirTarjeta cuando se ejecute  el evento onkey
-function restringirTarjeta(){//estta función toma el valor y valida su formato por una expresion regular
- numeroDeTarjeta.value = numeroDeTarjeta.value.replace(/\D/g, '');//esta expresión no permite ingresar letras ni expresiones (#$/+) 
-
-}
-console.log(numeroDeTarjeta);
-
 
 
 let botonValidar=document.getElementById('validate');//esta variable llama el id 'validate' de nuestro html
@@ -20,10 +7,10 @@ botonValidar.onclick= darClick;//le damos el evento 'onnclick' a esta variable p
 
 function darClick(){//esta función se activa con el evento 'onclick' toma el valor y lo procesa en la función isValid
   let creditCardNumber = document.getElementById('numeroDeTarjeta').value; //con esta variable llamamos  el valor ingresado en el id 'numeroDeTarjeta'
- // let nombre = document.getElementById('nombre');
-  //console.log(nombre)
   let tarjetaEsValida = validator.isValid(creditCardNumber);//la variable tarjetaesvalida se crea para guiar el valor a la carpeta validator. y ejecutar la funcion isValid(algoritmo de Luhn) 
 }
+
+
 
 
 document.getElementById("user").addEventListener("keyup",digitarNombre); //Llamamos el id'user' del html y le asignamos un evento 'keyup' 
@@ -35,24 +22,25 @@ function digitarNombre(){//se crea esta función que permite reflejar el valor e
   
 }
 
+let numeroDeTarjeta =document.getElementById("numeroDeTarjeta");// creamos una variable para llamar el valor del id de numeroDeTarjeta.
+numeroDeTarjeta.addEventListener("keyup", restringirTarjeta)// traemos el valor llamado para que cumpla la funcion restringirTarjeta cuando se ejecute  el evento onkey
+function restringirTarjeta(){//estta función toma el valor y valida su formato por una expresion regular
+ numeroDeTarjeta.value = numeroDeTarjeta.value.replace(/\D/g, '');//esta expresión no permite ingresar letras ni expresiones (#$/+) 
+
+}
+
 
 document.getElementById("numeroDeTarjeta").addEventListener("keyup", digitarNumeroTar) //Llamamos el id'numeroDeTarjeta' del html y le asignamos un evento 'keyup'
 function digitarNumeroTar(){//se crea esta función que permite reflejar el valor en una nueva ubicación (tarjeta)
   let numberCard= document.getElementById("numeroDeTarjeta");//esta variable llama el valor del ID'numeroDeTarjeta'
   let numeroTarjeta =numberCard.value//esta variable guarda el valor del ID'numeroDeTarjeta'
-  let repl=  numeroTarjeta.replace(/([0-9]{4})/g,'$1 ');// en la variable repl ingresa el numero de tarjeta y con replace reemplazamos o se coloca espacio en cada 4 números. 
+  let repl= validator.maskify(numeroTarjeta)
   let lblValue = document.getElementById("numTar");//esta variable llama el ID 'numTar' para ingresarlo en el label
   lblValue.innerText = repl;//este label imprime el valor guardado en el ID'numTar' y da el formato de la expresion regular
 }
 
 
-document.getElementById("numeroDeTarjeta").addEventListener("keyup",maskify);//Llamamos el id'numeroDeTarjeta' del html y le asignamos un evento 'keyup'
-function maskify(){//se crea esta función que permite reflejar el valor en una nueva ubicación (tarjeta)
-  let edValue = document.getElementById("numeroDeTarjeta");//esta variable llama el valor del ID'numeroDeTarjeta'
-  let s = edValue.value//esta variable guarda el valor del ID'numeroDeTarjeta'
-  let repl= s.replace(/.(?=.{4})/g, "#");// en la variable repl ingresa el numero de tarjeta y con replace  se reemplaza con # todos los digitos de la tarjeta excepto los últimos 4
-  let lblValue = document.getElementById("numTar"); //esta variable llama el ID 'numTar' para ingresarlo en el label
-  lblValue.innerText = repl; //este label imprime el valor guardado en el ID'numTar' y da el formato de la expresion regular
-}
+
+
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,9 +1,17 @@
 const validator = {
   isValid,
+  maskify,
   
 };
 
+ 
+ function maskify(creditCardNumber){//se crea esta función que permite reflejar el valor en una nueva ubicación (tarjeta)
+ let maskifyString= creditCardNumber.replace(/.(?=.{4})/g, "#");//esta variable toma creditCardNumber y reemplaza su formato con la expresion regular dada
 
+ return maskifyString
+ }
+
+  
 
 function invertirNumero(numero){//esta funcion invierte el numero ingresado
   
@@ -22,25 +30,21 @@ function isValid(number){// la función isValid, hace referencia al algoritmo de
   let reverseArrayNumber = invertirNumero(number);// esta variable nos permite traer el nuestros numeros invertidos
 
   for (let i = 0 ; i <= reverseArrayNumber.length -1 ; i++) { //Se crea un for para tomar cada valor de nuestra array
-    //console.log("posicion --->", i , ' val ->' , reverseArrayNumber[i]) 
     
-    let posicion= i+1;
+    let posicion= i+1;//se crea una variable donde sumamos una posicion para tome el primer digito como posicion 1 en el array
     let validarnumeros = posicion%2; //residuo de la posicion dividido en 0 (0 par 1 impar)
      
     if(validarnumeros === 0 ){ //condicionamos el valor de nuestro residuo que es igual a cero y nos indica las posiciones pares
-     // console.log('la posicion : ', i , 'es PAR');
          
       let valor = reverseArrayNumber[i]; //en la varible valor identficamos las nuevas posiciones 
 
       let valorx2 = valor*2; // los nuevos valores que cumplen las posiciones pares se multiplican por dos
               
       if(valorx2 >= 10){// Si, la multiplicacion de los valores pares son mayor o igual a 10 se debe desglosar el numero y sumar
-        // console.log(valorx2)
+
         let num1 = Number(valorx2.toString().split('')[0]);//se suma num1 con
         let num2 = Number(valorx2.toString().split('')[1]);//num 2
 
-       // console.log('num1 -> ', num1);
-        //console.log('num2 -> ', num2);
               
         let sumaNume= num1 + num2; // resultado de los numeros desglozados
         reverseArrayNumber[i] = sumaNume; // llos resultados de la suma se posicionan en la reverseArrayNumber
@@ -50,11 +54,8 @@ function isValid(number){// la función isValid, hace referencia al algoritmo de
       }
          
     }
-   // console.log("Posicion "+ i +" valor --> "+ reverseArrayNumber[i]);
 
   }
-  //console.log("Array con validacion de pares --> ", reverseArrayNumber);
-  //console.log("Array pos 2 -->" , arrayNumber[2] )
 
   let resultadoSuma = 0;// Se crea la variabe resultadoSuma para guardar la suma total de la array
   for (let i = 0; i < reverseArrayNumber.length; i++){
@@ -64,8 +65,6 @@ function isValid(number){// la función isValid, hace referencia al algoritmo de
   console.log(resultadoSuma);
   
   let resultadoFinal=resultadoSuma%10; // la variable resultado final, guarda el resultado de la suma total obtenida y se realiza una dvisión sintetica para obtener el multiplo de 10
-  //console.log('resultado suma -> ', resultadoSuma);
-  //console.log('resultado Final -> ', resultadoFinal);
 
   if (resultadoFinal%10===0){//Si cumple la condición el resultado será true
    console.log(true);
